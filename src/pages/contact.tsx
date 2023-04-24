f6d6f1a709ac724084c117c779775e4e708dc09c
Christine Kinya <kinyachristine14@gmail.com>
2023-04-24 22:18:52 +0300

    fixed a warning

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -23,6 +23,7 @@ export default function Contact() {
 
       <Box py={12} px={"28"} fontFamily={"Inconsolata"}>
         <iframe
+        title="Google Maps"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1021150.2078797459!2d35.54065927812499!3d-1.2505607999999946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f19ad9a1a2ce1%3A0xe96ada7314fbafe2!2sThe%20Andys%20Veterinary%20Clinic%20-%20Loresho!5e0!3m2!1sen!2ske!4v1682259206310!5m2!1sen!2ske"
           width="100%"
           height="450"
